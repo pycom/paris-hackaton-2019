4d63c2168cfb025042d80baec24dabb0c6f05aec
Ahmad EL Masri <ahmad@pycom.io>
2019-11-29 15:18:03 +0100

    feat: add reading-sensor project

---
+++ reading-sensor/main.py
@@ -0,0 +1,20 @@
+import pycom
+import time
+from machine import Pin
+from dth import DTH
+import _thread
+
+th = DTH(Pin('P23', mode=Pin.OPEN_DRAIN),0)
+time.sleep(2)
+
+def send_env_data():
+    while True:
+        result = th.read()
+        while not result.is_valid():
+            time.sleep(.5)
+            result = th.read()
+        pybytes.send_signal(1, result.temperature)
+        pybytes.send_signal(2, result.humidity)
+        time.sleep(10)
+
+_thread.start_new_thread(send_env_data, ())
